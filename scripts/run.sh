cf8ddb1f077566d331628d25608fd9f599403339
Voathnak Lim <voathnak.lim@gmail.com>
2020-06-05 01:33:45 +0700

    IMPR improve speed of running locally

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 source scripts/config.sh
-
+rm -rf .aws-sam
 sam build && \
-sam local start-api --region "$REGION" --profile "$PROFILE"
+sam local start-api --region "$REGION" --profile "$PROFILE"  --skip-pull-image --debug-port 5890
