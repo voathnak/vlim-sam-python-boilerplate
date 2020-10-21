75177ee20655813ceb145d93e830df956c89dcd2
Voathnak Lim <voathnak.lim@gmail.com>
2020-10-21 13:48:15 +0700

    init new project for testing

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-APP_NAME=vlim-co-sam-python-boilerplate
+APP_NAME=vlim-test-dynamodb-query
 STAGE_NAME=dev
 S3_BUCKET=$APP_NAME-$STAGE_NAME-bucket
 STACK_NAME=$APP_NAME-$STAGE_NAME-stack
-PROFILE=voathnakl
+PROFILE=private.vlim
 REGION=us-east-1
 INPUT_FILE=template.yaml
 OUTPUT_FILE=packaged.yaml
