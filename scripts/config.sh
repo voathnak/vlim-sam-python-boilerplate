3493e98a44629766f50912e39432ce0a6a0b3caa
Voathnak Lim <voathnak.lim@gmail.com>
2020-12-12 13:12:39 +0700

    made database name dynamic to app name and stage name

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-APP_NAME=vlim-co-sam-python-mongo-boilerplate
+APP_NAME=vlim-react-sam-python-mongo-boilerplate
 STAGE_NAME=dev
 S3_BUCKET=$APP_NAME-$STAGE_NAME-bucket
 STACK_NAME=$APP_NAME-$STAGE_NAME-stack
@@ -10,6 +10,6 @@ REGION=us-east-1
 INPUT_FILE=template.yaml
 OUTPUT_FILE=packaged.yaml
 DB_PASSWORD="bLgOTWoUvI4kaUtN"
-DB_NAME="vlim-co-sam-python-boilerplate"
+DB_NAME=$APP_NAME-$STAGE_NAME-mongodb
 DATABASE_URI="mongodb+srv://vlim:${DB_PASSWORD}@cluster0.qzg3k.mongodb.net/${DB_NAME}?retryWrites=true&w=majority"
 
